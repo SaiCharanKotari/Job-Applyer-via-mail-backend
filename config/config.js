562a70650cbee76b9be08ef9221093b99c3a6204
SaiCharanKotari <saicharankotari@gmail.com>
2025-10-27 23:32:05 +0530

    port things

--- config/config.js
+++ config/config.js
@@ -5,6 +5,10 @@ const db = sql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: true
+  },
 });
 
 db.connect((err) => {
