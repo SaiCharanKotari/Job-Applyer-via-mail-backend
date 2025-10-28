fd6000cd228378eea30123004a1342109424e816
SaiCharanKotari <saicharankotari@gmail.com>
2025-10-28 17:09:49 +0530

    comit of frontURL

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const mongoDB = require('./config/mDB')
 mongoDB();
 
 app.use(cors({
-  origin: 'https://job-applyer-via-mail-frontend.vercel.app/',
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
