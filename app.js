bf2551a835faf6e7de68a7174e25a35ad0b98a17
SaiCharanKotari <saicharankotari@gmail.com>
2025-10-28 17:43:20 +0530

    comit of frontURL

--- app.js
+++ app.js
@@ -3,21 +3,25 @@ dotenv.config();
 const express = require('express');
 const router = require('./routers/routes');
 const cors = require('cors');
-const cookie = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 const mongoDB = require('./config/mDB')
 mongoDB();
 
 app.set('trust proxy', true);
 app.use(express.json());
-app.use(cookie());
+app.use(cookieParser());
 app.use(cors({
-  origin: ['https://job-applyer-via-mail-frontend.vercel.app', 'https:localhost:5173'],
+  origin: [
+    'https://job-applyer-via-mail-frontend.vercel.app',
+    'http://localhost:5173'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   exposedHeaders: ['Set-Cookie']
 }));
+
 app.use('/', router);
 
 app.listen(process.env.PORT || 5000, () => {
